540f9e68ca8106fb37e1c24062578fed0cf05310
Priya Sharma <priyasharma15@gmail.com>
2021-04-05 23:14:44 +1000

    deploying code

--- public/index.js
+++ public/index.js
@@ -136,7 +136,7 @@ function sendTransaction(isAdding) {
   })
   .catch(err => {
     // fetch failed, so save in indexed db
-    saveRecord(transaction);
+    saveRecord(transactions);
 
     // clear form
     nameEl.value = "";
